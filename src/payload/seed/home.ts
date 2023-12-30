b356e970d76c00a6d9ba6fb462cc1cf853a279f9
BJR-developer <freelancerbjr@gmail.com>
2023-12-30 23:22:28 +0600

    ready for productions except env

--- src/payload/seed/home.ts
+++ src/payload/seed/home.ts
@@ -5,8 +5,8 @@ export const home: Partial<Page> = {
   slug: 'home',
   _status: 'published',
   meta: {
-    title: 'Payload E-Commerce Template',
-    description: 'An open-source e-commerce store built with Payload and Next.js.',
+    title: 'Neorative- Ecommerce website',
+    description: 'Full customizable ecommerce web',
     image: '{{PRODUCT1_IMAGE}}',
   },
   hero: {
